8a5c13f7a0f45844c2578dce1f6d5a3178cc6663
Anne Choulet <anne.choulet@free.fr>
2024-11-07 11:35:44 +0100

    Feature: upload images non redimensionnées

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -2,17 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\Services;
+use App\Form\AddServiceFormType;
+use App\Repository\ServicesRepository;
+use App\Service\ImageUploader;
+use App\Service\PictureServiceNorm;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ServicesController extends AbstractController
 {
     #[Route('/services', name: 'app_services')]
-    public function index(): Response
+    public function index(ServicesRepository $servicesRepository): Response
     {
-        return $this->render('services/index.html.twig', [
-            'controller_name' => 'ServicesController',
-        ]);
+        $services = $servicesRepository->findAll();
+
+        return $this->render('services/index.html.twig', compact('services'));
     }
+
+    #[Route(path: '/services/ajouter', name: 'app_services_add')]
+    public function addservice(Request $request, EntityManagerInterface $em, PictureServiceNorm $pictureServiceNorm): Response
+    {
+        $service = new Services();
+        $serviceForm = $this->createForm(AddServiceFormType::class, $service);
+
+        $serviceForm->handleRequest($request);
+
+        if ($serviceForm->isSubmitted() && $serviceForm->isValid()) {
+            $image = $serviceForm->get('image')->getData();
+            $imageLoad = $pictureServiceNorm->upload($image, 'services');
+            $service->setImage($imageLoad);
+
+            $em->persist($service);
+            $em->flush();
+
+            $this->addFlash('succes', 'service ajouté avec succès');
+
+            return $this->redirectToRoute('app_espace_admin');
+        }
+
+        return $this->render('services/add.html.twig', [
+            'serviceForm' => $serviceForm->createView(),
+        ]);
+
+    }
+
 }
